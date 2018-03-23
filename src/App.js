0ade3cae0cfef332ad17e93a9201f1ceb51d886f
sgueck9 <sgueck_23@hotmail.com>
2018-03-23 11:40:42 -0600

    dogCreated page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
-import Photos from './photos'
-import './photos.css'
-import Photospage from './components/photospage/photospage'
-import Profile from './components/Profile/Profile'
-import Message from './components/Message/Message'
+import Photos from './photos';
+import './photos.css';
+import Photospage from './components/photospage/photospage';
+import Profile from './components/Profile/Profile';
+import Message from './components/Message/Message';
 import Login from './components/Login/Login';
 import AddDogInfo from './components/AddDogInfo/AddDogInfo';
 import Swiping from './components/Swiping/Swiping';
 import UploadImage from './components/UploadImage/UploadImage';
-
-
 import Matches from './components/Matches/Matches';
 import Settings from './components/Settings/Settings';
-
-
-
-
-import EditInfo from './components/EditInfo/EditInfo'
+import EditInfo from './components/EditInfo/EditInfo';
+import DogCreated from './components/DogCreated/DogCreated'
 
 
 class App extends Component {
@@ -38,6 +33,7 @@ class App extends Component {
             <Route path='/message/:userOne/:userTwo' component={Message} />
             <Route path='/profile' component={Profile}/>
             <Route path='/editinfo' component={EditInfo}/>
+            <Route path='/dogcreated' component={DogCreated}/>
           </Switch>
        
       </div>
