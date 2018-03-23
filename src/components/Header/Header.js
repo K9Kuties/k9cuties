050abb68d01a0c45fcc75fde0a3473788436a2cc
tannerkaysmith <34726916+tannerkaysmith@users.noreply.github.com>
2018-03-23 10:56:35 -0600

    More styling, added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Header.css';
+import PoochPals from '../../@0.5xpoochpals.svg';
+
+
+class Header extends Component {
+
+
+    render() {
+        return (
+            <header className='pooch_pals_header' >
+                <img src={PoochPals} className='poochpals_svg_header' />
+            </header>
+        )
+    }
+}
+
+export default Header;
