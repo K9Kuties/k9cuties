40dfe31476fa0a22a60fa26cc87adaf5158b7cf6
Matt Johnston <matthew.ryan.johnston@gmail.com>
2018-03-27 12:08:37 -0600

    added functionality for range slider in settings component, made it so if you already have a dog saved in the db that you go straight to swiping, added lat/long in db to every dog so we can use reverse geolocation

--- db/submit_new_dog.sql
+++ db/submit_new_dog.sql
@@ -1,3 +1,3 @@
-insert into dogs (user_id, name, description, reason, interested_in, breed, age, gender, img1, img2, img3, img4, img5, img6, radius, location, age_begin, age_end)
-values($1, $2, null, 'Play dates', 'Both', $3, $4, $5, null, null, null, null, null, null, 20, $6, 0, 20)
+insert into dogs (user_id, name, description, reason, interested_in, breed, age, gender, img1, img2, img3, img4, img5, img6, radius, location, age_begin, age_end, latitude, longitude)
+values($1, $2, null, 'Play dates', 'Both', $3, $4, $5, null, null, null, null, null, null, 20, $6, 0, 20, $7, $8)
 returning *;
