ecaad0b79e515d2a1f7a4f39129feecf09358df4
sgueck9 <sgueck_23@hotmail.com>
2018-04-03 09:27:03 -0600

    profile

--- src/components/ItsAMatch/ItsAMatch.js
+++ src/components/ItsAMatch/ItsAMatch.js
@@ -2,3 +2,11 @@ import react, {Componenent} from 'react'
 import './ItsAMatch.css'
 import { connect } from 'react-redux'
 import { getUser, getDog } from './../../ducks/users'
+
+class ItsAMatch extends Componenent {
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
