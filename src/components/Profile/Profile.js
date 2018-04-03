edd2a8485427780cd27d28d404326f8d5df51448
tannerkaysmith <34726916+tannerkaysmith@users.noreply.github.com>
2018-04-02 19:36:45 -0600

    profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,19 +1,66 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Profile.css'
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getUser, getDog } from './../../ducks/users';
+import paw2 from '../../paw (2).svg'
 
-export default class Profile extends Component {
-  render(){
-      return(
-          <div className='profDiv'>
-              <div className='profilePic'>UR PICTURE HERE</div>
-              <div className='nameAndAge'>AXEL, 42</div>
-              <div>
-                <button className='settingsBut'>Settings</button>
-                <button className='editBut'>Edit</button>
-              </div>
-              <a href='http://localhost:3005/logout' ><button className='logBut' >Logout</button></a>
+class Profile extends Component {
+    constructor() {
+        super()
+        this.state = {
+            pictureImg: ''
+        }
+    }
 
-          </div>
-      )
-  }
+    componentDidMount() {
+        axios.get('/auth/me').then(res => {
+            this.props.getUser(res.data.user);
+            axios.get(`/api/getDog/${res.data.user.id}`).then(res => {
+                this.props.getDog(res.data[0])
+                this.setState({
+                    pictureImg: res.data[0].img1
+                })
+            })
+        })
+    }
+
+
+    render() {
+        return (
+            <div className='profDiv'>
+
+                <div className='profile_header' >
+                    <h1 className='invisible' ></h1>
+                    <h1 className='profile_header_h1' >Profile</h1>
+                    <Link to='/profile' ><img className='paw2' src={paw2} alt='back arrow logo' /></Link>
+                </div>
+
+                <div className='profile_information' >
+                    <img className='profilePic' src={this.state.pictureImg} alt='Profile Pic' />
+                    <div className='NameAndAge' >
+                        <div className='nameAnd'>{this.props.dog.name}, </div>
+                        <div className='ageAnd' >{this.props.dog.age}</div> 
+                    </div>
+                </div>
+
+                <div className='profile_buttons' >
+                    <Link to='/editinfo' ><button className='editBut'>Edit</button></Link>
+                    <Link to='/settings' ><button className='profileBut'>Settings</button></Link>
+                </div>
+
+                <a href='http://localhost:3005/logout' ><button className='profile_logBut' >Logout</button></a>
+
+            </div>
+        )
+    }
 }
+
+function mapStateToProps(state) {
+    return {
+        dog: state.dog
+    }
+}
+
+export default connect(mapStateToProps, { getUser, getDog })(Profile);
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -130,9 +130,9 @@ class Settings extends Component {
                 </div>
                 <form className='male_or_female'>
                     <h5 className='interested_in' >Interested in</h5>
-                    <input className='radio_button' type='radio' name='process' value='male' id='male' checked={this.state.selectedType === 'Male'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='male' >Male</label>
-                    <input className='radio_button' type='radio' name='process' value='female' id='female' checked={this.state.selectedType === 'Female'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='female' >Female</label>
-                    <input className='radio_button' type='radio' name='process' value='both' id='both' checked={this.state.selectedType === 'Both'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='both' >Both</label>
+                    <input className='radio_button' type='radio' name='process' value='Male' id='male' checked={this.state.selectedType === 'Male'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='male' >Male</label>
+                    <input className='radio_button' type='radio' name='process' value='Female' id='female' checked={this.state.selectedType === 'Female'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='female' >Female</label>
+                    <input className='radio_button' type='radio' name='process' value='Both' id='both' checked={this.state.selectedType === 'Both'} onChange={this.handleInterestedInChange} /><label className='male_or_female_label' for='both' >Both</label>
                 </form>
                 <div className='age_range' >
                     <h2 className='age_range_h2' >Age range </h2>
