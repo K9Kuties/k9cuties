118830db3d5f8546f0fc53197829532ab57623d4
Matt Johnston <matthew.ryan.johnston@gmail.com>
2018-04-04 07:28:08 -0600

    added query parameters to swipe array .get to allow for age filter

--- src/components/Swiping/Swiping.js
+++ src/components/Swiping/Swiping.js
@@ -31,7 +31,7 @@ class Swiping extends Component {
       this.props.getUser(res.data.user);
       axios.get(`/api/getDog/${res.data.user.id}`).then(res => {
         this.props.getDog(res.data[0])
-        let swipeArray = axios.get(`/api/getSwipeArray?id=${res.data[0].dog_id}&latitude=${res.data[0].latitude}&longitude=${res.data[0].longitude}&radius=${res.data[0].radius}&interested_in=${res.data[0].interested_in}&reason=${res.data[0].reason}`).then(res => {
+        let swipeArray = axios.get(`/api/getSwipeArray?id=${res.data[0].dog_id}&latitude=${res.data[0].latitude}&longitude=${res.data[0].longitude}&radius=${res.data[0].radius}&interested_in=${res.data[0].interested_in}&reason=${res.data[0].reason}&age_begin=${res.data[0].age_begin}&age_end=${res.data[0].age_end}`).then(res => {
           this.setState({
             swipeArray: res.data
           })
